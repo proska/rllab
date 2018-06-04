bcb7e15bbc69f20e355da1a3d63a12d1fa8618f2
eric-heiden <Eric13992>
2018-06-04 18:47:46 +0000

    add JointParameterized to tf/core/__init__.py

--- sandbox/rocky/tf/core/__init__.py
+++ sandbox/rocky/tf/core/__init__.py
@@ -1,4 +1,5 @@
 from sandbox.rocky.tf.core.parameterized import Parameterized
+from sandbox.rocky.tf.core.parameterized import JointParameterized
 from sandbox.rocky.tf.core.layers_powered import LayersPowered
 from sandbox.rocky.tf.core import layers
 from sandbox.rocky.tf.core.network import MLP
