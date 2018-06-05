c963b8762cfc1cc4cd483087d0443d73f5e081d0
Jonathon Shen <jshen100@gmail.com>
2018-06-05 16:50:38 -0700

    Readded rllab.spaces

--- rllab/spaces/__init__.py
+++ rllab/spaces/__init__.py
@@ -1,4 +1,6 @@
 from rllab.spaces.base import Space
 from rllab.spaces.product import Product
+from rllab.spaces.discrete import Discrete
+from rllab.spaces.box import Box
 
-__all__ = ["Product"]
+__all__ = ["Product", "Discrete", "Box"]
